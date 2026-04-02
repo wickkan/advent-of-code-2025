dacbfa1ed523aca4b511ec1dd2b8ff8d8b578673
Kanishka <kanishkawickramasinghe50@gmail.com>
2026-04-02 16:01:12 +1100

    cleaned up code

---
+++ day-1/main_p1.cpp
@@ -0,0 +1,38 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <string_view>
+
+const int MOD{100};
+
+int main() {
+  std::ifstream file{"data.txt"};
+  std::string move{};
+
+  // if we can't open the data for reading
+  if (!file) {
+    std::cerr << ("data.txt could not be opened for reading");
+  }
+
+  int dial{50};
+  int answer{};
+
+  while (std::getline(file, move)) {
+
+    char direction{move[0]};
+    int rotation{std::stoi(move.substr(1, move.length()))};
+
+    if (direction == 'L') // turn left
+    {
+      dial = (dial - rotation + MOD) % MOD;
+    } else if (direction == 'R') // turn right
+    {
+      dial = (dial + rotation) % MOD;
+    }
+
+    answer += (dial == 0);
+  }
+
+  std::cout << answer << "\n";
+  return 0;
+}
